83ac8bd20b21685fa7d49de6b07f7be2bc0a1059
idan94 <idan.cohen@campus.technion.ac.il>
2020-05-18 21:50:46 +0300

    Pass compilation(with make file)

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 all: clean
-	flex scanner.lex
+	flex scanner.flex
 	bison -d parser.ypp
 	g++ -std=gnu++0x -o hw3 *.c *.cpp
 clean:
-	rm -f flex.yy.c
+	rm -f lex.yy.c
 	rm -f parser.tab.*pp
 	rm -f hw3
---
+++ utils.hpp
@@ -0,0 +1,13 @@
+#ifndef __UTILS_HW3__
+#define __UTILS_HW3__
+
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+enum Enum_Type {T_INT,T_BYTE,T_BOOL,T_STRING};
+#endif
